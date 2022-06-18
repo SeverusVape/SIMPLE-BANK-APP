755d50788146b987f55bbc779834ac7c4125e5ef
Sergei Koshelev <koshelevworkmail@gmail.com>
2022-06-17 20:50:36 -0400

    Bag fixes, module includ.

--- script.js
+++ script.js
@@ -1,39 +1,10 @@
 "use strict";
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // BANKIST APP
 
-// Data
-const account1 = {
-    owner: "Sergei Koshelev",
-    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    interestRate: 1.2, // %
-    pin: 1111,
-};
+import { accounts } from "./database.mjs";
 
-const account2 = {
-    owner: "Jessica Davis",
-    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-    interestRate: 1.5,
-    pin: 2222,
-};
-
-const account3 = {
-    owner: "Steven Thomas Williams",
-    movements: [200, -200, 340, -300, -20, 50, 400, -460],
-    interestRate: 0.7,
-    pin: 3333,
-};
-
-const account4 = {
-    owner: "Sarah Smith",
-    movements: [430, 1000, 700, 50, 90],
-    interestRate: 1,
-    pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+//const accounts = [account1, account2, account3, account4];
 
 // Elements
 const labelWelcome = document.querySelector(".welcome");
@@ -183,7 +154,7 @@ btnTransfer.addEventListener("click", function (e) {
 // REQUEST LOAN
 btnLoan.addEventListener("click", (e) => {
     e.preventDefault();
-    const amount = Number(inputLoanAmount.value);
+    const amount = Math.floor(inputLoanAmount.value);
     if (
         amount > 0 &&
         currentAccount.movements.some((mov) => mov >= amount / 10)
@@ -208,7 +179,7 @@ btnClose.addEventListener("click", function (e) {
             (acc) => acc.username === currentAccount.username
         );
 
-        //DELETE ACCOUNT
+        //DELETEing ACCOUNT
         alert("YOUR ACCOUNT WILL BE DELETED!");
         accounts.splice(index, 1);
 
